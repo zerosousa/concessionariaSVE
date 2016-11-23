2484f59b092eaa0de20a065c8253d60c38362616
vitorarins <vitorarins@gmail.com>
2016-11-23 21:03:35 -0200

    Added list display to moto admin.

--- concessionaria/admin.py
+++ concessionaria/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+class MotoAdmin(admin.ModelAdmin):
+    list_display = ('placa')
+
 admin.site.register(Bairro)
 admin.site.register(Cidade)
 admin.site.register(Cliente)
@@ -12,7 +15,7 @@ admin.site.register(Local)
 admin.site.register(Logradouro)
 admin.site.register(Marca)
 admin.site.register(Modelo)
-admin.site.register(Moto)
+admin.site.register(Moto, MotoAdmin)
 admin.site.register(Ordemservico)
 admin.site.register(Produto)
 admin.site.register(Servico)
