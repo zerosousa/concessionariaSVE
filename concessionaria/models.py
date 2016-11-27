4087428a6e62d121799c832018d3e836aeadaada
vitorarins <vitorarins@gmail.com>
2016-11-27 00:16:35 -0200

    Added view to the models.

--- concessionaria/models.py
+++ concessionaria/models.py
@@ -219,5 +219,6 @@ class NfDetalhada(models.Model):
 
     class Meta:
         db_table = 'vw_nf_detalhada'
+        unique_together = ('cd_chassi', 'placa', 'nu_ordem')
         managed = False
         
